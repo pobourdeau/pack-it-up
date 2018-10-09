fd3b97548ab7945bf89509b9ecf2834530299019
POBGit <36237912+POBGit@users.noreply.github.com>
2018-10-09 10:56:34 -0400

    Modif

---
+++ Assets/Scripts/DeplacementPerso.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementPerso : MonoBehaviour {
+
+    private Rigidbody rbPerso;
+    private Animator animPerso;
+
+    public float vitesseDeplacement = 10f;
+    public float vDeplacement;
+    public float vRotation;
+
+
+	// Use this for initialization
+	void Start () {
+        rbPerso = GetComponent<Rigidbody>();
+        animPerso = GetComponent<Animator>();
+	}
+
+    void FixedUpdate() {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * vRotation, 0);
+
+        vDeplacement = Input.GetAxis("Vertical") * vitesseDeplacement;
+
+        print(vDeplacement);
+
+        rbPerso.velocity = (transform.forward * vDeplacement) + new Vector3(0, rbPerso.velocity.y, 0);
+
+
+    }
+}
